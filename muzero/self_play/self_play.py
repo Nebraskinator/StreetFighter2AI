aee85ce31a49dd0fccd1c49def4903216539a3c8
Nebraskinator <80223951+Nebraskinator@users.noreply.github.com>
2021-03-07 15:58:15 -0800

    Update self_play.py

--- muzero/self_play/self_play.py
+++ muzero/self_play/self_play.py
@@ -27,7 +27,6 @@ def run_selfplay(config: MuZeroConfig, storage: SharedStorage, game_name: str):
 
 
 def run_eval(config: MuZeroConfig, storage: SharedStorage, eval_episodes: int):
-    """Evaluate MuZero without noise added to the prior of the root and without softmax action selection"""
     network = storage.latest_network()
     returns = []
     for _ in range(eval_episodes):
@@ -40,7 +39,7 @@ def run_eval(config: MuZeroConfig, storage: SharedStorage, eval_episodes: int):
 def play_game(config: MuZeroConfig, network: AbstractNetwork, train: bool = True, game_name: str = 'default_agent') -> AbstractGame:
     """
     Each game is produced by starting at the initial board position, then
-    repeatedly executing a Monte Carlo Tree Search to generate moves until the end
+    repeatedly executing a learned search to select moves until the end
     of the game is reached.
     """
     overworlds = ['1-1','2-1','3-1','3-2','4-1','5-1','5-2','6-1','6-2','7-1','8-1','8-2','8-3']
@@ -48,20 +47,21 @@ def play_game(config: MuZeroConfig, network: AbstractNetwork, train: bool = True
     athletics = ['1-3','2-3','3-3','4-3','5-3','6-3','7-3']
     waterworlds = ['2-2','7-2']
     castles = ['1-4', '2-4', '3-4', '5-4', '6-4']
-    game = config.new_game(overworlds)
+    game = config.new_game(overworlds) # Make a new game from one in the list
     mode_action_select = 'softmax' if train else 'max'
     noop = random.randint(0,config.initial_random_moves)
     while not game.terminal() and len(game.history) < config.max_moves:
-        # At the root of the search tree we use the representation function to
+        # We use the representation function to
         # obtain a hidden state given the current observation.
         current_observation = game.make_image(-1)
         inital_network_output = network.initial_inference(current_observation)
+        # We evaluate the value of config.num_searches moves to
+        # get our current ideal policy
         action_values = value_search(config, network, inital_network_output)
 
-        # We then run a Monte Carlo Tree Search using only action sequences and the
-        # model learned by the networks.
+        # Set the root value of the current state
         game.root_values.append(np.mean(list(action_values.values())))
-        
+        # Normalize the values of our ideal policy
         min_value = sorted(action_values, key=action_values.get)[0]
         min_value = action_values[min_value]
         max_value = sorted(action_values, key=action_values.get)[-1]
@@ -75,9 +75,9 @@ def play_game(config: MuZeroConfig, network: AbstractNetwork, train: bool = True
             
         target_sum = sum(target_policy)
         target_policy = [i/target_sum for i in target_policy]
-        
+        # Save the current ideal policy
         game.child_visits.append(target_policy)
-        
+        # Select action to perform using our ideal policy as probabilities
         if len(game.history) < noop:
             action = select_action(config, len(game.history), normalized_action_values, mode='random')
         else:
@@ -88,6 +88,10 @@ def play_game(config: MuZeroConfig, network: AbstractNetwork, train: bool = True
     return game
 
 def value_search(config: MuZeroConfig, network: AbstractNetwork, inital_network_output):
+    # Performs a search for the ideal move by taking the top
+    # config.num_searches moves from our policy network and
+    # predicting their value config.search_depth moves into the
+    # future
     search_policy = inital_network_output.policy_softmax
     ranked_actions = sorted(search_policy, key=search_policy.get, reverse=True)
     actions_to_evaluate = ranked_actions[:config.num_searches]
